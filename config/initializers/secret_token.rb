af75fd24e8d5b22720c01d5cb7d0121d188c09cf
Rob Day-Reynolds <rdr@Obsidian.local>
2012-05-29 21:26:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomoreboysclub::Application.config.secret_token = '9eff8058a5f580ef4352bc0c219e97209dbbb67727bd7f69a7d6b878c1148bea7a165af312bf050defcf4830542a3e623237517a8cd230d05399fe167c247c2b'
